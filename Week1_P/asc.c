8f24e44063ee82c564ce3987bfbabd1e4b1e863c
Pascal Goldbrunner <pascal.goldbrunner@tum.de>
2018-10-25 10:30:58 +0200

    Commit of the exercises from the first week

---
+++ Week1_P/asc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+        printf("Oct\tDec\tHex\tChar\n");
+	for(unsigned char ascii=0;ascii<128;ascii++){
+		printf("%03o\t%u\t%02x\t%c\n", ascii, ascii, ascii, ascii);
+	}
+}
---
+++ Week1_P/hd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void hexdump(FILE *output, char *buffer, int length);
+
+int main(int argc, char** argv){
+        if(argc <= 1){
+		printf("Puffer angeben!!\n");
+		return 0;
+	}
+
+	for(int i=1;i<argc;i++){
+		hexdump(stdout, argv[i], strlen(argv[i])+1);
+		printf("\n");
+	}
+}
