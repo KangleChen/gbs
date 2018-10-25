44e264c225d25c935810c95ccc24cfd92125ab1e
Pascal Goldbrunner <pascal.goldbrunner@tum.de>
2018-10-25 10:53:36 +0200

    Formatting

--- Week1_P/hd.c
+++ Week1_P/hd.c
@@ -3,14 +3,14 @@
 
 void hexdump(FILE *output, char *buffer, int length);
 
-int main(int argc, char** argv){
-        if(argc <= 1){
-		printf("Puffer angeben!!\n");
-		return 0;
-	}
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        printf("Puffer angeben!!\n");
+        return 0;
+    }
 
-	for(int i=1;i<argc;i++){
-		hexdump(stdout, argv[i], strlen(argv[i])+1);
-		printf("\n");
-	}
+    for (int i = 1; i < argc; i++) {
+        hexdump(stdout, argv[i], strlen(argv[i]) + 1);
+        printf("\n");
+    }
 }
--- Week1_P/rotup.c
+++ Week1_P/rotup.c
@@ -1,22 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char** argv){
-        char in[128];
-	ssize_t len = read(0, in, 128);
-	in[len-1] = '\0';
+int main(int argc, char **argv) {
+    char in[128];
+    ssize_t len = read(0, in, 128);
+    in[len - 1] = '\0';
 
-	char rot13[len+1];
-	int i=0;
-	char c = in[i];
-	while((c>='A' && c<='Z') || (c>='a' && c<='z')){
-		if(c>='a' && c<='z'){
-			c = c-'a'+'A';
-		}
-		rot13[i] = (((c-'A')+13)%26)+'A';
-		i++;
-		c = in[i];
-	}
-	rot13[i] = '\0';
-	printf("Hallo: %s -- %s\n", in, rot13);
+    char rot13[len + 1];
+    int i = 0;
+    char c = in[i];
+    while ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        if (c >= 'a' && c <= 'z') {
+            c -= 'a' + 'A';
+        }
+        rot13[i] = (((c - 'A') + 13) % 26) + 'A';
+        i++;
+        c = in[i];
+    }
+    rot13[i] = '\0';
+    printf("Hallo: %s -- %s\n", in, rot13);
 }
