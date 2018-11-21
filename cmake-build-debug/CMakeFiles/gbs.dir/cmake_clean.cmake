d663cd0a0aced605a4c6a376c7eb296d0b1f8dfa
Tung Nguyen <nguyetun@in.tum.de>
2018-11-21 16:43:02 +0100

    Added CMake configuration files. Mainly for Jol not having to config the CMakeList for debugging

---
+++ cmake-build-debug/CMakeFiles/gbs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gbs.dir/Week5/syncem.c.o"
+  "gbs.pdb"
+  "gbs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gbs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
