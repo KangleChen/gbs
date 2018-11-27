43bfd0a05b2cc51b90f6b96c89f301284312fe45
Pascal Goldbrunner <pascal.goldbrunner@tum.de>
2018-11-27 21:13:20 +0100

    blub

--- cmake-build-debug/CMakeFiles/list.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/list.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/list.dir/Week5/list.c.o"
-  "liblist.pdb"
-  "liblist.a"
+  "CMakeFiles/List.dir/Week5/list.c.o"
+  "libList.pdb"
+  "libList.a"
 )
 
 # Per-language clean rules from dependency scanning.
 foreach(lang C)
-  include(CMakeFiles/list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+  include(CMakeFiles/List.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
