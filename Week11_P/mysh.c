7926a2229b704a78eed4c1af82f93a7add9696ff
Modac <modac@gmx.net>
2019-01-21 14:32:09 +0100

    Week 11 commentary

--- Week11_P/mysh.c
+++ Week11_P/mysh.c
@@ -13,14 +13,21 @@
 #include "list.h"
 #include "wildcard.h"
 
+// fake directory name to idetify internal commands
 char internalPrefixFormat[] = "/>%06d#internal</";
+
+// The resulting string whith length:
 // snprintf(NULL, 0, internalPrefixFormat, 0) + 1
 char internalPrefix[20];
 
+// Current working directory as string
 char *pwd;
 
+// Thread list (they aren't actual threads so I don't remember why i called it that)
 list_t *tL;
+// Pipe descriptor list (again, I don't remember why it's cL)
 list_t *cL;
+// List of parameters given to the shell
 list_t *paras;
 
 // parser.c function declarations
@@ -36,6 +43,7 @@ int intPcmp(const void *intP1, const void *intP2) {
     return (*(int *) intP1) - (*(int *) intP2);
 }
 
+// Frees 'all' allocated memory and exits.
 void cleanExit(int status) {
     list_finit(tL);
     list_finit(cL);
@@ -44,34 +52,62 @@ void cleanExit(int status) {
     exit(status);
 }
 
+// Change the current working directory by the given list of parameters
+// e.g. list ["cd"] -> ["a/dir/to/go"]
+// the first element of the list should always be the string "cd".
+// It can be something else but it does get ignored none the less.
+// Always returns 0
 int changeDir(list_t *cmdList) {
     int argc = list_length(cmdList);
+    // no arguments given
     if (argc == 1) {
+        // print current working dir
         printf("%s\n", pwd);
-    } else if (argc == 2) {
-        char *dir = (char *)cmdList->first->next->data;
+    } // exactly one argument given
+    else if (argc == 2) {
+        // the new dir string is the second argument
+        char *dir = (char *) cmdList->first->next->data;
+
+        // the new working dir is: pwd + "/" + dir. So the length must be strlen(pwd) + 1 + strlen(dir) + 1 (null term)
         char newPwd[strlen(pwd) + strlen(dir) + 2];
         sprintf(newPwd, "%s/%s", pwd, dir);
+
+        // get the real path of the new wd (/path/.././path/.. -> /) in other words deuglify it
+        // but because
         char realNewPwd[4096];
         realpath(newPwd, realNewPwd);
         //printf("RealNewPwd: %s\n", realNewPwd);
+
+        // check if the given path exists and is a directory
         struct stat s;
         int err = stat(newPwd, &s);
         if (err != -1 && S_ISDIR(s.st_mode)) {
+
+            // copy the realNewPwd to our current working dir
             strcpy(pwd, realNewPwd);
             //printf("pwd: %s\n", pwd);
+            // set the environmental var PWD to the new wd
             setenv("PWD", pwd, 1);
+            // also directly set the the wd for the current process
             chdir(pwd);
+
         } else {
+            // Error: file or directory not found
             printf("cd: %s: %s\n", dir, strerror(ENOENT));
         }
-    } else {
+        // we don't have to free anything because we only declared arrays :)
+    } // too many arguments or list is empty
+    else {
+        // Print usage
         printf("Usage: cd <dir>\n");
     }
     return 0;
 }
 
 int execCmd(const char *filename, char *const argv[], char *const envp[]) {
+    // I actually wanted to delegate the internal process execution to this method but this method gets called after
+    // fork() so we can't change anything in the actual shell process
+    // TODO: Cleanup
     /*
     if (strncmp(filename, internalPrefix, strlen(internalPrefix)) == 0) {
         if (strncmp(filename + strlen(internalPrefix), "/cd", 3) == 0) {
@@ -84,34 +120,49 @@ int execCmd(const char *filename, char *const argv[], char *const envp[]) {
 
 
 int processCmd(list_t *tL, list_t *cmdList, char *envp[], int inPipe, list_t *cL) {
+
+    // check if cd is called
+    // maybe delegate this check to another function like 'processInternalCmd'
+    if (list_length(cmdList) > 0 && strncmp((char *) cmdList->first->data, "cd", 2) == 0) {
+        return changeDir(cmdList);
+    }
+
     char **outFileP = malloc(sizeof(char *));
     char **inFileP = malloc(sizeof(char *));
     int cpid;
 
-    if(list_length(cmdList) > 0 && strncmp((char *)cmdList->first->data, "cd", 2) == 0){
-        return changeDir(cmdList);
-    }
 
     list_t *args2 = list_init();
 
 
     int pipeA2[2];
+    // look for next pipe
     if (myParsePipe(cmdList, args2, pipeA2) >= 0) {
+
+        // add the output fd of the pipe to the pipe list
         int *cP = malloc(sizeof(int));
         *cP = pipeA2[0];
         list_append(cL, cP);
+
+        // add the input fd of the pipe to the pipe list
         cP = malloc(sizeof(int));
         *cP = pipeA2[1];
         list_append(cL, cP);
+
+        // process the command after the pipe first
         processCmd(tL, args2, envp, pipeA2[0], cL);
-    } else {
+    } // no pipes no worries :)
+    else {
         pipeA2[0] = -1;
         pipeA2[1] = -1;
     }
 
+    // look for in-/out-redirs
     myParseStg2(cmdList, outFileP, inFileP);
 
     if ((cpid = fork()) == 0) {
+
+        // convert the param list to the argv array for execve
         char *argv[list_length(cmdList) + 1];
         list_to_array(cmdList, (void **) argv);
         argv[list_length(cmdList)] = NULL;
@@ -130,13 +181,16 @@ int processCmd(list_t *tL, list_t *cmdList, char *envp[], int inPipe, list_t *cL
         sprintf(string, "%s:%s", internalPrefix, path);
         */
 
+        // if we can pipe our output
         if (pipeA2[1] > 0) {
-            dup2(pipeA2[1], STDOUT_FILENO);
+            dup2(pipeA2[1], STDOUT_FILENO);    // do it
         }
+        // if we can get our input from a pipe
         if (inPipe > 0) {
-            dup2(inPipe, STDIN_FILENO);
+            dup2(inPipe, STDIN_FILENO);    // do it
         }
 
+        // we don't need any pipe fd anymore, because we already dup'd them. So close them all
         while (cL->first != NULL) {
             struct list_elem *cC = cL->first;
             close(*((int *) cC->data));
@@ -146,19 +200,24 @@ int processCmd(list_t *tL, list_t *cmdList, char *envp[], int inPipe, list_t *cL
 
         int fd = -1;
 
+        // if we can output to a file
         if (*outFileP != NULL && (fd = open(*outFileP, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) >= 0) {
-            dup2(fd, STDOUT_FILENO);
+            dup2(fd, STDOUT_FILENO);    // do it
             close(fd);
         }
+        // if we can get input from a file
         if (*inFileP != NULL && (fd = open(*inFileP, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) >= 0) {
-            dup2(fd, STDIN_FILENO);
+            dup2(fd, STDIN_FILENO);    // do it
             close(fd);
         }
 
+        // if the command contains a slash -> execute as is
         if (strchr(argv[0], '/') != NULL) {
             execCmd(argv[0], argv, envp);
-        } else {
+        } // if not -> search for executable in PATH
+        else {
 
+            //try every PATH entry
             ptr = strtok(string, delimiter);
 
             while (ptr != NULL) {
@@ -171,10 +230,12 @@ int processCmd(list_t *tL, list_t *cmdList, char *envp[], int inPipe, list_t *cL
                 ptr = strtok(NULL, delimiter);
             }
 
+            // if a valid executable was found execCmd (execve) doesn't return
             fprintf(stderr, "Command '%s' not found\n", argv[0]);
             cleanExit(-1);
         }
     } else {
+        // add the child command process to the 'tread' list (maybe it's task list)
         int *cpidP = malloc(sizeof(int));
         *cpidP = cpid;
         list_append(tL, cpidP);
@@ -185,36 +246,47 @@ int processCmd(list_t *tL, list_t *cmdList, char *envp[], int inPipe, list_t *cL
 
 int main(int argc, char *argv[], char *envp[]) {
 
+    // generate the internalPrefix
+    // obsolete
+    // TODO: remove, cleanup
     srand(time(NULL));
     sprintf(internalPrefix, internalPrefixFormat, rand() % 900000 + 100000);
 
-    char str[1024];
+    // input buffer
+    char str[4096];
 
     tL = list_init();
     cL = list_init();
     paras = list_init();
 
+    // set the current working dir of our shell (pwd) to the current working dir of the process
     pwd = calloc(4096, sizeof(char));
-    //printf("pwd size: %d\n", (int) (sizeof(pwd) / sizeof(pwd[0])));
     if (getcwd(pwd, 4096) == NULL) {
+        // if not found, just asume we're in th root dir
         sprintf(pwd, "/");
     }
 
     printf("%s $ ", pwd);
+    // because we didn't print a new line the our printf might not have been actually printed
     fflush(stdout);
-    while (fgets(str, 1024, stdin) != NULL) {
-        if (paras != NULL) {
-            myParse(paras, str, envp);
-        }
+    while (fgets(str, 4096, stdin) != NULL) {
+        // if the command is "exit" -> exit the shell
         if (strcmp(str, "exit\n") == 0) {
             cleanExit(0);
         }
 
+        // parse the input string to a list of params
+        if (paras != NULL) {
+            myParse(paras, str, envp);
+        }
+
+        // expand all wildcards
         expandWildcards(paras);
 
+        // process the command
         processCmd(tL, paras, envp, -1, cL);
 
-
+        // close all pipe fds (because we only closed them in the child processes)
         while (cL->first != NULL) {
             struct list_elem *cC = cL->first;
             close(*((int *) cC->data));
@@ -222,7 +294,7 @@ int main(int argc, char *argv[], char *envp[]) {
             list_remove(cL, cC);
         }
 
-
+        // wait for all processes to finish
         int cpid;
         while (tL->first != NULL) {
             cpid = wait(NULL);
--- Week11_P/wildcard.c
+++ Week11_P/wildcard.c
@@ -10,10 +10,15 @@
 
 char *wildcardStr;
 
+// the function which acts as a filter for scandir
 int scanFilter(const struct dirent *dirEnt) {
+
+    // ignore "." and ".."
     if (strcmp(dirEnt->d_name, ".") == 0 || strcmp(dirEnt->d_name, "..") == 0) {
         return 0;
     }
+
+    // wildcard matching through the neat fnmatch function
     if (fnmatch(wildcardStr, dirEnt->d_name, 0) == 0) {
         return 1;
     }
@@ -21,21 +26,38 @@ int scanFilter(const struct dirent *dirEnt) {
 
 }
 
+// takes every parameter and checks for wildcards
+// then it adds every possible match as an own param
+// e.g.
+// ["ls"] -> ["/home/stud*/hw"]
+// =>
+// ["ls"] -> ["/home/student/hw"] -> ["/home/student2/hw"]
 void expandWildcards(list_t *paras) {
     struct list_elem *curr = paras->first;
     for (int i = 0; curr != NULL; i++) {
         char *string = (char *) curr->data;
+
+        // contains wildcard ?
         if (strchr(string, '*') != NULL) {
 
+            // search for the last slash before the wildcard
+            // the wildcard itself
+            // and the next slash after the wildcard
+            // they're the index of the corresponding chars within the string (0 based)
+            //          "/home/stud*/hw" (len: 14)
+            // lastSlash(5)---|    ||---nextSlash(11)
+            //      wildcard(10)---|
             int lastSlash = -1;
             int wildcard = -1;
             int nextSlash = -1;
 
             for (int j = 0; j < strlen(string); ++j) {
                 if (string[j] == '/') {
+                    // if wildcard not found maybe its the lastSlash
                     if (wildcard < 0) {
                         lastSlash = j;
-                    } else {
+                    } // if wildcard already found it's the nextSlash
+                    else {
                         if (nextSlash < 0) {
                             nextSlash = j;
                             break;
@@ -45,7 +67,11 @@ void expandWildcards(list_t *paras) {
                     wildcard = j;
                 }
             }
-            // bla/bl*aet/tr.ml
+
+            // split the string into 3 parts:
+            // "/home/stud*/hw"
+
+            // prefix: the path before the part with the wildcard: "/home" (empty if lastSlash < 0)
             char *prefix;
             if (lastSlash < 0) {
                 prefix = calloc(1, sizeof(char));
@@ -54,6 +80,7 @@ void expandWildcards(list_t *paras) {
                 strncpy(prefix, string, lastSlash);
             }
 
+            // infix: the part with the wildcard in it: "stud*"
             char *infix;
             int inL;
             if (nextSlash < 0) {
@@ -65,6 +92,7 @@ void expandWildcards(list_t *paras) {
             }
             strncpy(infix, string + lastSlash + 1, inL);
 
+            // suffix: the path after the part with the wildcard: "hw" (empty if nextSlash < 0)
             char *suffix;
             if (nextSlash < 0) {
                 suffix = calloc(1, sizeof(char));
@@ -74,19 +102,29 @@ void expandWildcards(list_t *paras) {
                 strncpy(suffix, string + nextSlash + 1, sufL);
             }
 
+            // scan all dirs in prefix or if prefix is empty in the current dir (".")
             wildcardStr = infix;
 
             struct dirent **namelist;
             int n;
 
-            n = scandir(strlen(prefix)==0?".":prefix, &namelist, scanFilter, alphasort);
+            n = scandir(strlen(prefix) == 0 ? "." : prefix, &namelist, scanFilter, alphasort);
             if (n < 0) {
                 // ERROR
             } else {
                 while (n--) {
-                    char *tempStr = calloc(strlen(namelist[n]->d_name) + strlen(prefix) + strlen(suffix) + 1,
+
+                    // concat the prefix with the found dir and the suffix (optional slashes in between)
+                    char *tempStr = calloc(strlen(prefix) + (prefix[0] == '\0' ? 0 : 1) +
+                                           strlen(namelist[n]->d_name) +
+                                           (suffix[0] == '\0' ? 0 : 1) + strlen(suffix) +
+                                           1,
                                            sizeof(char));
-                    sprintf(tempStr, "%s%s%s%s%s", prefix, strlen(prefix) == 0?"":"/" , namelist[n]->d_name, strlen(suffix) == 0?"":"/" , suffix);
+
+                    sprintf(tempStr, "%s%s%s%s%s",
+                            prefix, prefix[0] == '\0' ? "" : "/",
+                            namelist[n]->d_name,
+                            suffix[0] == '\0' ? "" : "/", suffix);
 
                     /*
                     char *realPath = calloc(4096, sizeof(char));
@@ -97,11 +135,15 @@ void expandWildcards(list_t *paras) {
                     free(realPath);
                     */
 
+
+                    //check if this path actually exists
                     struct stat s;
                     int err = stat(tempStr, &s);
                     if (err != -1) {
+                        // yes it does -> good to go (be added to the params list)
                         list_insert_after(paras, curr, tempStr);
                     } else {
+                        // meehh -> discard
                         free(tempStr);
                     }
 
@@ -110,8 +152,10 @@ void expandWildcards(list_t *paras) {
                 free(namelist);
             }
             struct list_elem *next = curr->next;
+            // remove wildcard parameter
             free(string);
             list_remove(paras, curr);
+
             curr = next;
         } else {
             curr = curr->next;
